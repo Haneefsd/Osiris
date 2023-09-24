41a6fd6cba184c9f724a0c716928d86dab92c7bd
Haneefsd <141896565+Haneefsd@users.noreply.github.com>
2023-09-24 20:39:48 +0530

    to-dolist.js

--- to-dolist.js
+++ to-dolist.js
@@ -1,34 +1,25 @@
-// Get references to HTML elements
 const inputBox = document.getElementById('input-box');
 const listContainer = document.getElementById('list-container');
 
-// Function to add a task
 function addTask() {
   const taskText = inputBox.value.trim();
 
   if (taskText !== '') {
     const listItem = document.createElement('li');
     listItem.textContent = taskText;
-
-    listItem.addEventListener('click', toggleTask);
-    
+    listItem.addEventListener('click', toggleTask);    
     listContainer.appendChild(listItem);
-    
     inputBox.value = '';
   }
 }
 
-// Function to toggle the status of a task (checked/unchecked)
 function toggleTask(event) {
   const listItem = event.target;
   listItem.classList.toggle('checked');
 }
 
-// Event listener for the "Add" button
 const addButton = document.querySelector('button');
 addButton.addEventListener('click', addTask);
-
-// Event listener for the Enter key
 inputBox.addEventListener('keypress', function (event) {
   if (event.key === 'Enter') {
     addTask();
